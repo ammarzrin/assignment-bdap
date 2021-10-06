8e1734e07394af3346a093bfd2cdfeda3092395c
ammarzrin <ammarzrin@gmail.com>
2021-10-06 12:13:17 +0800

    added main.cpp which is our program source code

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/* Program Information */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Function prototypes go here.
+
+
+// Main Program
+int main()
+{
+	
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+// Functions are defined below this comment.
