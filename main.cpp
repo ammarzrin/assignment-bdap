310524d4294862a960e6044a7de346545ab9ca79
ammarzrin <ammarzrin@gmail.com>
2021-11-09 02:29:37 +0800

    interface design is updated

--- main.cpp
+++ main.cpp
@@ -217,21 +217,24 @@ int main()
 
 void displayMainMenu(string &user, string &file)
 {
-	cout << "=---------------------------------=" << endl;
-	cout << "|   Basic Data Analysis Program   |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "|         -> Main Menu <-         |" << endl;
-	cout << "=---------------------------------=" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|               Basic Data Analysis Program                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|                     -> Main Menu <-                      |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << endl;
 	cout << "Welcome, " << user << "." << endl;
 	cout << "Current file: " << file << endl;
 	cout << endl;
-	cout << "What would you like to do?" << endl;
-	cout << "    1. Load New Data File" << endl;
-	cout << "    2. Rename Current File" << endl;
-	cout << "    3. Perform Statistical Analysis" << endl;
-	cout << "    4. Generate a Report" << endl;
-	cout << "    5. Manage my Account" << endl;
-	cout << "    6. Exit program" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|  What would you like to do?                              |" << endl;
+	cout << "|      1. Load New Data File                               |" << endl;
+	cout << "|      2. Rename Current File                              |" << endl;
+	cout << "|      3. Perform Statistical Analysis                     |" << endl;
+	cout << "|      4. Generate a Report                                |" << endl;
+	cout << "|      5. Manage my Account                                |" << endl;
+	cout << "|      6. Exit program                                     |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
 	cout << "Select an integer from 1 to 6" << endl;
 	cout << "---> ";
@@ -240,19 +243,22 @@ void displayMainMenu(string &user, string &file)
 void fileInputMenu(string &user, string &file, bool &loaded)
 {
 	string input;
-	cout << "=---------------------------------=" << endl;
-	cout << "|   Basic Data Analysis Program   |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "|      -> New File Input <-       |" << endl;
-	cout << "=---------------------------------=" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|               Basic Data Analysis Program                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|                   -> New File Input <-                   |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << endl;
 	cout << "Logged in as " << user << "." << endl;
 	cout << endl;
-	cout << "Guidelines:" << endl;
-	cout << " >  Before performing other functions, please load a data file for the program to analyze." << endl;
-	cout << " >  File must be in the same directory as the program." << endl;
-	cout << " >  This program reads data files in (.txt) format." << endl;
-	cout << " >  Input example: file name.txt" << endl;
-	cout << " >  To cancel operation and go back to main menu, input a dash (-)." << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "| Guidelines:                                              |" << endl;
+	cout << "|  >  Load a data file for the program to analyze.         |" << endl;
+	cout << "|  >  File must be in the same directory as the program.   |" << endl;
+	cout << "|  >  Program reads data files in (.txt) format.           |" << endl;
+	cout << "|  >  Input example: file name.txt                         |" << endl;
+	cout << "|  >  Input a dash (-) to cancel operation.                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
 	cout << "Enter file name ---> ";
 	cin.ignore();
@@ -269,19 +275,22 @@ void fileInputMenu(string &user, string &file, bool &loaded)
 void fileRenameMenu(string &user, string &file)
 {
 	string input;
-	cout << "=---------------------------------=" << endl;
-	cout << "|   Basic Data Analysis Program   |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "|    -> Rename Current File <-    |" << endl;
-	cout << "=---------------------------------=" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|               Basic Data Analysis Program                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|                -> Rename Current File <-                 |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << endl;
 	cout << "Logged in as " << user << "." << endl;
 	cout << "Current file: " << file << endl;
 	cout << endl;
-	cout << "Guidelines:" << endl;
-	cout << " >  Renaming a file will save the loaded file with a new name." << endl;
-	cout << " >  If the file name already exists, you will have to input another file name." << endl;
-	cout << " >  Input example: 'file name.txt'" << endl;
-	cout << " >  To cancel operation and go back to main menu, input a dash (-)." << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "| Guidelines:                                              |" << endl;
+	cout << "|  >  Replace current file name with a new name.           |" << endl;
+	cout << "|  >  File name cannot be the same as an existing file.    |" << endl;
+	cout << "|  >  Input example: file name.txt                         |" << endl;
+	cout << "|  >  Input a dash (-) to cancel operation.                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
 	cout << "Enter a new name for current file ---> ";
 	cin.ignore();
@@ -294,25 +303,28 @@ void fileRenameMenu(string &user, string &file)
 
 void displayStatMenu(string &user, string &file)
 {
-	cout << "=---------------------------------=" << endl;
-	cout << "|   Basic Data Analysis Program   |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "|   -> Statistical  Analysis <-   |" << endl;
-	cout << "=---------------------------------=" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|               Basic Data Analysis Program                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|                -> Statistical Analysis <-                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << endl;
 	cout << "Logged in as " << user << "." << endl;
 	cout << "Current file: " << file << endl;
 	cout << endl;
-	cout << "What would you like to calculate from your file?" << endl;
-	cout << "    1. Find Minimum" << endl;
-	cout << "    2. Find Maximum" << endl;
-	cout << "    3. Median Value" << endl;
-	cout << "    4. Mean Value (Average)" << endl;
-	cout << "    5. Variance" << endl;
-	cout << "    6. Standard Deviation" << endl;
-	cout << "    7. Correlation Between 2 Columns" << endl;
-	cout << "    8. Distinct Data Members" << endl;
-	cout << "    9. Plot a Histogram" << endl;
-	cout << "   10. Go Back to Main Menu" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|  What would you like to calculate from your file?        |" << endl;
+	cout << "|      1. Find Minimum                                     |" << endl;
+	cout << "|      2. Find Maximum                                     |" << endl;
+	cout << "|      3. Median Value                                     |" << endl;
+	cout << "|      4. Mean Value (Average)                             |" << endl;
+	cout << "|      5. Variance                                         |" << endl;
+	cout << "|      6. Standard Deviation                               |" << endl;
+	cout << "|      7. Correlation Between 2 Columns                    |" << endl;
+	cout << "|      8. Distinct Data Members                            |" << endl;
+	cout << "|      9. Plot a Histogram                                 |" << endl;
+	cout << "|     10. Go Back to Main Menu                             |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
 	cout << "Select an integer from 1 to 10" << endl;
 	cout << "---> ";
@@ -320,22 +332,24 @@ void displayStatMenu(string &user, string &file)
 
 void displayReportMenu(string &user, string &file, string &report)
 {
-	cout << "=---------------------------------=" << endl;
-	cout << "|   Basic Data Analysis Program   |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "|     -> Report  Generator <-     |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "Logged in as " << user << "." << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|               Basic Data Analysis Program                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|                  -> Report Generator <-                  |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
+	cout << "Logged in as " << user << "." << endl;
 	cout << "Report for " << file << " is generated below:" << endl;
 	cout << endl;
 	cout << report;
 	cout << endl;
-	cout << "What would you like to do now?" << endl;
-	cout << "    1. Save Report as text file (.txt)" << endl;
-	cout << "    2. Save Report as HTML file (.html)" << endl;
-	cout << "    3. Go Back to Main Menu" << endl;
-	cout << "    4. Exit Program" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|  What would you like to do now?                          |" << endl;
+	cout << "|      1. Save Report as text file (.txt)                  |" << endl;
+	cout << "|      2. Save Report as HTML file (.html)                 |" << endl;
+	cout << "|      3. Go Back to Main Menu                             |" << endl;
+	cout << "|      4. Exit Program                                     |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
 	cout << "Select an integer from 1 to 4" << endl;
 	cout << "---> ";
@@ -343,19 +357,22 @@ void displayReportMenu(string &user, string &file, string &report)
 
 void adminAccountSettings(string &user)
 {
-	cout << "=---------------------------------=" << endl;
-	cout << "|   Basic Data Analysis Program   |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "|     -> Account Settings <-      |" << endl;
-	cout << "=---------------------------------=" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|               Basic Data Analysis Program                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|                  -> Account Settings <-                  |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << endl;
 	cout << "Logged in as " << user << "." << endl;
 	cout << endl;
-	cout << "What would you like to do?" << endl;
-	cout << "    1. Change Password" << endl;
-	cout << "    2. Log Out" << endl;
-	cout << "    3. Create a New User" << endl;
-	cout << "    4. Delete an Existing User" << endl;
-	cout << "    5. Go Back to Main Menu" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|  What would you like to do?                              |" << endl;
+	cout << "|      1. Change Password                                  |" << endl;
+	cout << "|      2. Log Out                                          |" << endl;
+	cout << "|      3. Create a New User                                |" << endl;
+	cout << "|      4. Delete an Existing User                          |" << endl;
+	cout << "|      5. Go Back to Main Menu                             |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
 	cout << "Select an integer from 1 to 5" << endl;
 	cout << "---> ";
@@ -363,17 +380,20 @@ void adminAccountSettings(string &user)
 
 void buyerAccountSettings(string &user)
 {
-	cout << "=---------------------------------=" << endl;
-	cout << "|   Basic Data Analysis Program   |" << endl;
-	cout << "=---------------------------------=" << endl;
-	cout << "|     -> Account Settings <-      |" << endl;
-	cout << "=---------------------------------=" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|               Basic Data Analysis Program                |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|                  -> Account Settings <-                  |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << endl;
 	cout << "Logged in as " << user << "." << endl;
 	cout << endl;
-	cout << "What would you like to do?" << endl;
-	cout << "    1. Change Password" << endl;
-	cout << "    2. Log Out" << endl;
-	cout << "    3. Go Back to Main Menu" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
+	cout << "|  What would you like to do?                              |" << endl;
+	cout << "|      1. Change Password                                  |" << endl;
+	cout << "|      2. Log Out                                          |" << endl;
+	cout << "|      3. Go Back to Main Menu                             |" << endl;
+	cout << "=----------------------------------------------------------=" << endl;
 	cout << endl;
 	cout << "Select an integer from 1 to 3" << endl;
 	cout << "---> ";
